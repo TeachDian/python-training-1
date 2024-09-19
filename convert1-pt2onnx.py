d704f19dbf9a94c0c7e55f2ac04ad32a582629f2
TeachDian <philiptenibas@gmail.com>
2024-09-19 14:39:06 +0800

    some more models to try with

--- convert1-pt2onnx.py
+++ convert1-pt2onnx.py
@@ -2,7 +2,7 @@ import torch
 from ultralytics import YOLO  # If using YOLOv8
 
 # Step 1: Load the saved checkpoint, which contains the model directly
-checkpoint = torch.load('models/yolov8/150epoch.pt')
+checkpoint = torch.load('models/yolov8/240epoch.pt')
 
 # Step 2: Access the model directly from the checkpoint
 model = checkpoint['model']  # This directly gives you the model
@@ -10,11 +10,14 @@ model = checkpoint['model']  # This directly gives you the model
 # Step 3: Convert the model to full precision
 model = model.float()
 
-# Step 4: Set the model to evaluation mode
+# Step 4: Move the model to GPU
+model = model.to('cuda')
+
+# Step 5: Set the model to evaluation mode
 model.eval()
 
-# Step 5: Define an example input (full precision by default)
-dummy_input = torch.randn(1, 3, 640, 640)  # Adjust according to your input size
+# Step 6: Define an example input and move it to GPU
+dummy_input = torch.randn(1, 3, 640, 640).to('cuda')  # Adjust according to your input size
 
-# Step 6: Export the model to ONNX
+# Step 7: Export the model to ONNX
 torch.onnx.export(model, dummy_input, "model.onnx", opset_version=11)
